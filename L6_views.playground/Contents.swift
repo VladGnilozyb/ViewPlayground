88e66ed545328f22574136d04ee2a2d3a34a38e8
Vlad <vlad.gnilozyb@gmail.com>
2017-12-04 19:58:14 +0300

    vlad! change color

--- L6_views.playground/Contents.swift
+++ L6_views.playground/Contents.swift
@@ -10,8 +10,8 @@ class MyViewController : UIViewController {
 
         let label = UILabel()
         label.frame = CGRect(x: 150, y: 300, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
+        label.text = "Hello VLad!"
+        label.textColor = .green
         
         view.addSubview(label)
         self.view = view
